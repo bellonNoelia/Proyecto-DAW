f3b25d987a4926c72d70057cf4a2df45d1939f84
Noelia Bellon <noeliabellonlopez@gmail.com>
2022-10-27 20:07:16 +0200

    Search-bar in header

--- FrontEnd/PhotoRaw/src/app/app.module.ts
+++ FrontEnd/PhotoRaw/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { LoginControllerComponent } from './login-controller/login-controller.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-import { LoginControllerComponent } from './login-controller/login-controller.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +24,8 @@ import { LoginControllerComponent } from './login-controller/login-controller.co
     MatToolbarModule,
     MatIconModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
