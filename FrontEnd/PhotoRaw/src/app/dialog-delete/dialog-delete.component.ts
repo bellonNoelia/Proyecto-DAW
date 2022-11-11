b8d35875c159cb83a287689982b3c81e8c3c637a
Noelia Bellon <noeliabellonlopez@gmail.com>
2022-11-12 00:16:24 +0100

    Develop dialog-update and dialog-delete components

--- FrontEnd/PhotoRaw/src/app/dialog-delete/dialog-delete.component.ts
+++ FrontEnd/PhotoRaw/src/app/dialog-delete/dialog-delete.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Inject } from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-dialog-delete',
   templateUrl: './dialog-delete.component.html',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogDeleteComponent implements OnInit {
 
-  constructor() { }
-
+  message: string = "¿Está seguro de que deseas eliminar el producto?"
+  acceptBtnTxt = "Si"
+  cancelBtnTxt = "No"
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<DialogDeleteComponent>) {
+      if(data){
+    this.message = data.message || this.message;
+    if (data.buttonText) {
+      this.acceptBtnTxt = data.buttonText.ok || this.acceptBtnTxt;
+      this.cancelBtnTxt = data.buttonText.cancel || this.cancelBtnTxt;
+    }
+      }
+  }
   ngOnInit(): void {
   }
-
+  accept(): void {
+    this.dialogRef.close(true);
+  }
 }
--- FrontEnd/PhotoRaw/src/app/dialog-update/dialog-update.component.ts
+++ FrontEnd/PhotoRaw/src/app/dialog-update/dialog-update.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-update',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogUpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<DialogUpdateComponent>,
+    @Inject(MAT_DIALOG_DATA)
+    public message:string) { }
 
   ngOnInit(): void {
   }
-
+  close() {
+    this.dialogRef.close();
+  }
 }
