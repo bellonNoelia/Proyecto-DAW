e988a6983eb03312c51c767259ae9009f704dda1
Noelia Bellon <noeliabellonlopez@gmail.com>
2022-12-13 23:47:45 +0100

    Front updates

--- FrontEnd/PhotoRaw/src/app/photos/photos.component.ts
+++ FrontEnd/PhotoRaw/src/app/photos/photos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Photo} from '../model/photo';
+import { Photo } from '../model/photo';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogReadComponent } from '../dialog-read/dialog-read.component';
 import { Service } from '../service/service.service';
@@ -7,25 +7,29 @@ import { Service } from '../service/service.service';
 @Component({
   selector: 'app-photos',
   templateUrl: './photos.component.html',
-  styleUrls: ['./photos.component.css']
+  styleUrls: ['./photos.component.css'],
 })
-
 export class PhotosComponent implements OnInit {
   photos: Photo[];
+  columnas: number;
 
-  constructor(public dialog: MatDialog, public service:Service) { }
+  constructor(public dialog: MatDialog, public service: Service) {}
 
   ngOnInit(): void {
-    this.getAllPhotos()
+    this.getAllPhotos();
+    this.columnas = window.innerWidth <= 600 ? 1 : 6;
+  }
+  handleSize(event) {
+    this.columnas = event.target.innerWidth <= 600 ? 1 : 6;
   }
 
-  private getAllPhotos(){
-    this.service.findAll().subscribe(data => {
+  private getAllPhotos() {
+    this.service.findAll().subscribe((data) => {
       this.photos = data;
     });
   }
 
-  openDialogRead(photo:Photo) {
-    const dialogRef = this.dialog.open(DialogReadComponent, { data: photo});
+  openDialogRead(photo: Photo) {
+    const dialogRef = this.dialog.open(DialogReadComponent, { data: photo });
   }
 }
--- FrontEnd/PhotoRaw/src/app/profile/profile.component.ts
+++ FrontEnd/PhotoRaw/src/app/profile/profile.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { User} from '../model/user';
-import { Photo} from '../model/photo';
+import { Component, OnInit, Input } from '@angular/core';
+import { User } from '../model/user';
+import { Photo } from '../model/photo';
 
 import { Service } from '../service/service.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogCreateComponent } from '../dialog-create/dialog-create.component';
 import { DialogReadComponent } from '../dialog-read/dialog-read.component';
 import { DialogUpdateComponent } from '../dialog-update/dialog-update.component';
-
 
 import { ActivatedRoute } from '@angular/router';
 
@@ -17,66 +16,65 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
   artists: User;
   idUsuario: number;
-  usuarioLog:any;
-  photos : Photo[];
+  usuarioLog: any;
+  photos: Photo[];
   columnas: number;
 
   constructor(
     public dialog: MatDialog,
-    public service:Service,
-    private _Activatedroute: ActivatedRoute ) {}
+    public service: Service,
+    private _Activatedroute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.getArtists();
     this.userLog();
     this.findPhotosByUser();
-    this.columnas = (window.innerWidth <= 600) ? 1 : 6;
+    this.columnas = window.innerWidth <= 600 ? 1 : 6;
   }
   handleSize(event) {
-  this.columnas = (event.target.innerWidth <= 600) ? 1 : 6;
+    this.columnas = event.target.innerWidth <= 600 ? 1 : 6;
   }
 
-    private getArtists(){
-      this.service.getArtists(this.getParameter()).subscribe(data => {
-        this.artists = data;
-      });
-
-    }
-    private userLog(){
-      this.service.userLog().subscribe(data=>{
-        this.usuarioLog=data;
-      });
-    }
-    private findPhotosByUser(){
-      this.service.findPhotosByUser(this.getParameter()).subscribe(data=>{
-        this.photos=data;
-      })
-    }
+  private getArtists() {
+    this.service.getArtists(this.getParameter()).subscribe((data) => {
+      this.artists = data;
+    });
+  }
+  private userLog() {
+    this.service.userLog().subscribe((data) => {
+      this.usuarioLog = data;
+    });
+  }
+  private findPhotosByUser() {
+    this.service.findPhotosByUser(this.getParameter()).subscribe((data) => {
+      this.photos = data;
+    });
+  }
 
   openDialogCreate() {
-    const dialogRef = this.dialog.open(DialogCreateComponent, { data: new Object});
-
+    const dialogRef = this.dialog.open(DialogCreateComponent, {
+      data: new Object(),
+    });
   }
-  openDialogRead(photo:Photo) {
-   this.dialog.open(DialogReadComponent, { data: photo});
-  }
-
-  openDialogUpdate(photo:Photo) {
-     this.dialog.open(DialogUpdateComponent, {data:photo});
+  openDialogRead(photo: Photo) {
+    this.dialog.open(DialogReadComponent, { data: photo });
   }
 
- deletePhoto(photo) {
- this.service.deletePhoto(photo.id).subscribe(response => {
-  console.log(response);
-  this.findPhotosByUser();
-})
- }
-
-  private getParameter(){
-    return this._Activatedroute.snapshot.params["id"]
+  openDialogUpdate(photo: Photo) {
+    this.dialog.open(DialogUpdateComponent, { data: photo });
   }
 
+  deletePhoto(photo) {
+    this.service.deletePhoto(photo.id).subscribe((response) => {
+      console.log(response);
+      this.findPhotosByUser();
+    });
+  }
+
+  private getParameter() {
+    return this._Activatedroute.snapshot.params['id'];
+  }
 }
